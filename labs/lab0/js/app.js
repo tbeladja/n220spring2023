d4b2f12be1c4e79f7f723fbd1164fa2f4d53a283
tbeladja <tbeladja@iu.edu>
2023-01-12 12:53:41 -0500

    adding base lab structure

---
+++ labs/lab0/js/app.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(800, 600);
+    background(29, 92, 18);
+
+    fill(252, 45, 187);
+    rect(40, 50, 100, 20);
+}
+
+function draw() {
+    circle(mouseX, ,mouseY,12);
+}
