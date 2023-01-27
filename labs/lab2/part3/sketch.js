6ef497015defe81e635427c03c527cfa1024ba91
tbeladja <tbeladja@iu.edu>
2023-01-26 23:47:45 -0500

    updates lab2
    updates lab2

--- labs/lab2/part3/sketch.js
+++ labs/lab2/part3/sketch.js
@@ -1,3 +1,17 @@
+// Declare variables for x and y position of the circle, as well as xSpeed and ySpeed
+
+// In the setup function, create canvas with width of 800 and height of 600
+
+//In the draw function:
+
+  // Clear background
+  // Draw circle at current x and y position
+  // Update x and y position by adding corresponding speed variables
+  // Check if x position is at or past the width of the canvas (800) or if it is at or past 0
+  // If either of these conditions are met, multiply xSpeed by -1 to change direction of movement
+  // Check if y position is at or past the height of the canvas (600) or if it is at or past 0
+  // If either of these conditions are met, multiply ySpeed by -1 to change direction of movement
+
 let x = 400;
 let y = 300;
 let xSpeed = -5;
